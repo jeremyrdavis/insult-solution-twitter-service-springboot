0f1e9f1241409875f409355b0ba966dc63ce4a02
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2019-05-07 10:21:47 -0400

    working

--- src/test/java/com/redhat/summit2019/TwitterServiceTest.java
+++ src/test/java/com/redhat/summit2019/TwitterServiceTest.java
@@ -1,5 +1,6 @@
 package com.redhat.summit2019;
 
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,6 +29,8 @@ public class TwitterServiceTest {
         Map<String, String> insultBody = new HashMap<>();
         insultBody.put("insult", "Verily, ye be a pox-marked, rank blind-worm!");
 
+        RestAssured.baseURI = String.format("http://localhost:%d", port);
+
         Response response = given()
                 .contentType("application/json")
                 .body(insultBody)
@@ -37,10 +40,6 @@ public class TwitterServiceTest {
                 .extract().response();
         assertNotNull(response);
         System.out.println(response.toString());
-    }
-
-    protected String baseURI() {
-        return String.format("http://localhost:%d", port);
     }
 
 }
